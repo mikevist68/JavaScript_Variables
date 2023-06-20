78543b881d4df36e8550f8baef5f7b39f480dcf1
Michael Vistocci <mikevist68@yahoo.com>
2023-06-20 18:45:30 -0400

    completed

--- app.js
+++ app.js
@@ -4,7 +4,28 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Michael"
+let lastName = "Vistocci"
+var age = 37
 
 // Exercise 2
 
 // Exercise 3
+let language = "JavaScript"
+let createdYear = 1995
+let isCaseSensitive = true
+
+//exercise 4
+let price = 19.99
+let isONSale = false
+let salesPercentage = 15
+let stock = 0
+let inStock = false
+let selectedSize = "M"
+
+//Exercise 5
+let title = "Name of the Wind"
+let author = "Patrick Rothfuss"
+let pageCount = 722
+let bookmark = 456
+let hasRead = true
